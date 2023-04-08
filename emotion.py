c9aa6e2fe33cc2dbdabb6aa36301d4d383041030
Edwin Hati <52129921+edwinhati@users.noreply.github.com>
2023-04-08 13:32:16 +0000

    Pending changes exported from your codespace

--- emotion.py
+++ emotion.py
@@ -1,143 +1,63 @@
-happy = {
-    "Bagus": 1,
-    "bermanfaat": 1,
-    "berkualitas": 1,
-    "memenuhi ekspektasi": 1,
-    "membantu": 1,
-    "mudah": 1,
-    "praktis": 1,
-    "terpercaya": 1,
-    "aman": 1,
-    "lancar": 1,
-    "suka": 1,
-    "kece": 1,
-    "mantap": 1,
-    "senang": 1,
-    "terjamin": 1,
-    "good": 1,
-    "cepat": 1,
-    "tidak ribet": 1,
-    "luar biasa": 1,
-    "gampang": 1,
-    "top": 1,
-    "oke": 1,
-    "berguna": 1,
-    "nyaman": 1,
-    "keren": 1,
-    "baik": 1,
-    "puas": 1,
-    "berhasil": 1,
-    "untung": 1,
-    "nice": 1,
-    "excellent": 1,
-    "terbaik": 1,
-    "bravo": 1,
-    "best": 1,
-    "ok": 1,
-    "alhamdulillah": 1,
-}
-sad = {
-    "gagal": -1,
-    "susah": -1,
-    "gak bisa": -1,
-    "jaringan": -1,
-    "tidak bisa masuk": -1,
-    "gagal koneksi": -1,
-    "sedih": -1,
-    "tidak bisa": -1,
-    "gangguan": -1,
-    "shock": -1,
-    "hang": -1,
-    "layar kosong": -1,
-    "mengecewakan": -1,
-    "ga efektif": -1,
-    "ga ngerti": -1,
-    "lelet": -1,
-}
-angry = {
-    "banyak trouble": -1,
-    "masalah": -1,
-    "stress": -1,
-    "sulit": -1,
-    "susahnya minta ampun": -1,
-    "selalu gagal": -1,
-    "uninstall": -1,
-    "parah": -1,
-    "menyusahkan": -1,
-    "ga usah pake": -1,
-    "konyol": -1,
-    "komplain": -1,
-    "abal-abal": -1,
-    "aneh": -1,
-    "buruk": -1,
-    "jengkel": -1,
-    "sebal": -1,
-    "marah": -1,
-    "emosi": -1,
-    "hancur": -1,
-    "payah": -1,
-    "ga guna": -1,
-    "ribet": -1,
-    "rumit": -1,
-    "lemot(lambat)": -1,
-    "ga karuan": -1,
-    "cacat": -1,
-    "capek": -1,
-    "ga becus": -1,
-    "bangsat": -1,
-    "sampah": -1,
-    "malas": -1,
-    "miris": -1,
-}
-fear = {
-    "tidak jadi": -1,
-    "bocor": -1,
-    "error": -1,
-    "terblokir": -1,
-    "ga aman": -1,
-    "tidak aman": -1,
-    "reset": -1,
-    "root": -1,
-    "data hilang": -1,
-    "takut": -1,
-    "hilang": -1,
-    "saldo minus": -1,
-    "awas": -1,
-    "hati-hati": -1,
-    "penipu": -1,
-    "rugi": -1,
-}
+import pandas as pd
+import csv
+
+lexicon_positive = dict()
+lexicon_negative = dict()
+
+try:
+    with open('positive.tsv', 'r') as csvfile:
+        reader = csv.reader(csvfile, delimiter='\t')
+        next(reader)
+        for row in reader:
+            lexicon_positive[row[0]] = int(row[1])
+except FileNotFoundError:
+    print("Positive file not found")
+
+try:
+    with open('negative.tsv', 'r') as csvfile:
+        reader = csv.reader(csvfile, delimiter='\t')
+        next(reader)
+        for row in reader:
+            lexicon_negative[row[0]] = int(row[1])
+except FileNotFoundError:
+    print("Negative file not found")
 
 
 def emotion_analyst(text):
-    score = 0
+    emotion_score = 0
     for word in text:
-        if word in happy:
-            score += happy[word]
-        if word in sad:
-            score += sad[word]
-        if word in angry:
-            score += angry[word]
-        if word in fear:
-            score += fear[word]
-
-    if score >= 0:
+        if (word in lexicon_positive):
+            emotion_score = emotion_score + lexicon_positive[word]
+    for word in text:
+        if (word in lexicon_negative):
+            emotion_score = emotion_score + lexicon_negative[word]
+    emotion = ""
+    if (emotion_score > 0):
         emotion = "happy"
-    elif -2 <= score < 0:
-        emotion = "sad"
-    elif -5 <= score < -2:
-        emotion = "angry"
+    elif (emotion_score < 0):
+        if (emotion_score < -20):
+            emotion = "fear"
+        elif (emotion_score < -10):
+            emotion = "angry"
+        else:
+            emotion = "sad"
     else:
-        emotion = "fearful"
-    return score, emotion
-import pandas as pd
+        emotion = "undefined"
+    return emotion_score, emotion
+
+
 
 dataset = pd.read_csv('dataset.csv')
 dataset['text_preprocessed'] = dataset['text_preprocessed'].astype(str)
+
+for i, text in enumerate(dataset['text_preprocessed']):
+    dataset.at[i, 'text_preprocessed'] = text.replace("'", "")\
+        .replace(',', '').replace(']', '').replace('[', '').split()
+
 
 results = dataset['text_preprocessed'].apply(emotion_analyst)
 results = list(zip(*results))
 dataset['emotion_score'] = results[0]
 dataset['emotion'] = results[1]
 
-dataset.to_csv(r'emotion.csv', index=False, header= True, index_label=None)
+dataset.to_csv(r'emotion.csv', index=False, header=True, index_label=None)
